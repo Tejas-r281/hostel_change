2050170607dd64665656560787299521e38840f0
Raushan <raushan.043.kumar@gmail.com>
2022-03-12 20:30:16 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -25,10 +25,10 @@ const errorMiddleware = require("./middleware/error");
 
  app.use("/api/v1", user);
 
-//  app.use(express.static(path.join(__dirname, "../frontend/build")));
-//  app.get("*", (req, res, next) =>
-//    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
-//  );
+ app.use(express.static(path.join(__dirname, "../frontend/build")));
+ app.get("*", (req, res, next) =>
+   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+ );
 
 
 
