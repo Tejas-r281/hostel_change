fdc70cde568dbdc614715e1788900127a55c1a48
Raushan <raushan.043.kumar@gmail.com>
2022-05-07 17:39:12 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -25,12 +25,12 @@ const suggestion= require("./routes/suggestionRoute");
 
  app.use("/api/v1", user);
  app.use("/api/v1",suggestion);
- 
 
-//  app.use(express.static(path.join(__dirname, "../frontend/build")));
-//  app.get("*", (req, res, next) =>
-//    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
-//  );
+
+ app.use(express.static(path.join(__dirname, "../frontend/build")));
+ app.get("*", (req, res, next) =>
+   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+ );
 
 
 
